1307839d60a72e232dcd23197b331b91deee0980
Edgar Ayala <edgarayl10@gmail.com>
2026-03-05 07:21:32 -0600

    Homepage CSS finished

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello!');
+//Hamburger menu
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+//For rearranging html elements with media query
+const mobile = "(max-width: 768px)";
+function setFirstChild() {
+  const matched = window.matchMedia(mobile).matches;
+
+  const topElement = matched
+    ? document.getElementById("element-b")
+    : document.getElementById("element-a");
+
+  const parent = topElement.parentNode;
+  parent.insertBefore(topElement, parent.firstChild);
+}
+window.onload = setFirstChild;
+window.matchMedia(mobile).addEventListener("change", setFirstChild);
